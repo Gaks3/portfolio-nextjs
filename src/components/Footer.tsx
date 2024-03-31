37671760fb47bd9186abd80c4819971e1437e923
Gaks3 <dwi66116@gmail.com>
2024-03-31 15:48:26 +0700

    feat: footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className='mb-3 space-x-5 space-y-8 text-center'>
+      <span>AceternityUI</span>
+      <span>NextJS</span>
+    </footer>
+  )
+}
