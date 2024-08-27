53f44478278faa4565a4ca56d9f1aeabfd570eca
Gaks3 <dwi66116@gmail.com>
2024-08-27 23:29:03 +0700

    feat: add icon cloud and the icons

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,57 +1,41 @@
-import Image from 'next/image'
-
-type ImagesData = Array<{
-  url: string
-  alt: string
-}>
+import IconCloud from './ui/icon-cloud'
 
 export default function Skills() {
-  const images: ImagesData = [
-    {
-      url: 'javascript.png',
-      alt: 'Javascript Logo',
-    },
-    {
-      url: 'typescript.png',
-      alt: 'Typescript Logo',
-    },
-    {
-      url: 'nextjs.png',
-      alt: 'NextJS Logo',
-    },
-    {
-      url: 'nodejs.png',
-      alt: 'NodeJS Logo',
-    },
-    {
-      url: 'postgresql.png',
-      alt: 'PostreSQL Logo',
-    },
-    {
-      url: 'mysql.png',
-      alt: 'Mysql Logo',
-    },
-    {
-      url: 'tailwind.png',
-      alt: 'Tailwind Logo',
-    },
-    {
-      url: 'trpc.png',
-      alt: 'TRPC Logo',
-    },
+  const slugs = [
+    'typescript',
+    'javascript',
+    'react',
+    'android',
+    'html5',
+    'css3',
+    'nodejs',
+    'express',
+    'prisma',
+    'postgresql',
+    'firebase',
+    'nginx',
+    'vercel',
+    'git',
+    'github',
+    'visualstudiocode',
+    'androidstudio',
+    'figma',
+    'php',
+    'laravel',
+    'mysql',
+    'tailwindcss',
+    'bun',
+    'mqtt',
+    'nestjs',
+    'nvm',
+    'python',
+    'shadcnui',
+    'ubuntu',
   ]
 
   return (
     <div className='flex flex-row h-auto items-center justify-center flex-wrap gap-5'>
-      {images.map((image, id) => (
-        <Image
-          src={`/${image.url}`}
-          alt={image.alt}
-          width={100}
-          height={100}
-          key={id}
-        />
-      ))}
+      <IconCloud iconSlugs={slugs} />
     </div>
   )
 }
